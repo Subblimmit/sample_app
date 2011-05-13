bae7dbe595371b7fc75023d0933317fe5037af18
Subblimmit <subrange636@gmail.com>
2011-05-14 01:17:59 +0300

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
   describe PagesController do
     render_views
 
+    before(:each) do
+      @base_title = "Ruby on Rails Tutorial Sample App"
+    end
+
     describe "GET 'home'" do
 
       it "should be successful" do
@@ -13,12 +17,11 @@ require 'spec_helper'
       it "should have the right title" do
 	get 'home'
 	response.should have_selector("title",
-			  :content => "Ruby on Rails Tutorial Sample App | Home")
+				      :content => @base_title + " | Home")
       end
     end
 
     describe "GET 'contact'" do
-
       it "should be successful" do
 	get 'contact'
 	response.should be_success
@@ -27,13 +30,11 @@ require 'spec_helper'
       it "should have the right title" do
 	get 'contact'
 	response.should have_selector("title",
-			  :content =>
-			    "Ruby on Rails Tutorial Sample App | Contact")
+				      :content => @base_title + " | Contact")
       end
     end
 
     describe "GET 'about'" do
-
       it "should be successful" do
 	get 'about'
 	response.should be_success
@@ -42,8 +43,20 @@ require 'spec_helper'
       it "should have the right title" do
 	get 'about'
 	response.should have_selector("title",
-			  :content =>
-			    "Ruby on Rails Tutorial Sample App | About")
+				      :content => @base_title + " | About")
+      end
+    end
+    describe "GET 'home'" do
+
+      it "should be successful" do
+	get 'help'
+	response.should be_success
+      end
+
+      it "should have the right title" do
+	get 'help'
+	response.should have_selector("title",
+				      :content => @base_title + " | Help")
       end
     end
   end
