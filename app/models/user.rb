939a334b76b692fe1ac2a92bbcf513c1337a0931
Subblimmit <subrange636@gmail.com>
2011-05-21 23:49:00 +0300

    Hello

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,6 +44,11 @@ class User < ActiveRecord::Base
     (user && user.salt == cookie_salt) ? user : nil
   end
   
+  def feed
+    # This is preliminary. See Chapter 12 for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
+  
   private
 
   def encrypt_password
@@ -63,8 +68,4 @@ class User < ActiveRecord::Base
     Digest::SHA2.hexdigest(string)
   end
   
-  def feed
-    # This is preliminary. See Chapter 12 for the full implementation.
-    Micropost.where("user_id = ?", id)
-  end
 end
